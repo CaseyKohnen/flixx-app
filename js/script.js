2d0586b9cef5d84814c33f763bde057f54565e6a
CaseyKohnen <Casey.Kohnen@gmail.com>
2023-05-11 17:47:01 -0500

    swipper api added

--- js/script.js
+++ js/script.js
@@ -219,6 +219,51 @@ function displayBackgroundImage(type, backgroundPath){
   }
 }
 
+async function displaySlider(){
+  const {results} = await fetchAPIData('movie/now_playing');
+
+  results.forEach((movie) =>{
+    const div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    
+    div.innerHTML = `
+            <a href="movie-details.html?id=${movie.id}">
+              <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}" />
+            </a>
+            <h4 class="swiper-rating">
+              <i class="fas fa-star text-secondary"></i> ${movie.vote_average} / 10
+            </h4>
+    `;
+    document.querySelector('.swiper-wrapper').appendChild(div);
+
+    initSwiper();
+  })
+}
+
+function initSwiper(){
+  const swiper = new Swiper('.swipper', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    freeMode: true,
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false
+    },
+    breakpoints:{
+      500: {
+        slidesPerView: 2
+      },
+      700: {
+        slidesPerView: 3
+      },
+      1200: {
+        slidesPerView: 4
+      },
+    }
+  });
+}
+
 //Fetch Data
 async function fetchAPIData(endpoint){
     const API_KEY = '2d3dffeef2ab4141e45e743e98bab472';
@@ -261,6 +306,7 @@ function init(){
     switch(global.curretPage){
         case '/':
         case '/index.html':
+            displaySlider();
             displayPopularMovies();
             break;
         case '/shows.html':
